acd74f3835edf522acf1e91ccfca904e7e3dae38
ASHWINI SHAKTIVEL KUMAR <74529032+ashwinishaktive1@users.noreply.github.com>
2024-03-09 11:59:22 -0500

    Final script for data retrieval from all available terms in the Banner

--- node.py
+++ node.py
@@ -1,8 +1,17 @@
+"""
+Loading data into tabular form from the NEU Banner website using the NU Banner API.
+This data (in .csv) format can now be used as context for the RAG models.
+
+Author: Ashwini Shaktivel Kumar
+"""
 import requests
 import json
 import pandas as pd
 import time
 
+"""
+Defining options (arguments) for the API requests
+"""
 # Define the options for retrieving terms
 term_options = {
   'url': 'https://nubanner.neu.edu/StudentRegistrationSsb/ssb/classSearch/getTerms',
@@ -52,7 +61,7 @@ search_options = {
     'startDatepicker': '',
     'endDatepicker': '',
     'pageOffset': '',
-    'pageMaxSize': '200',
+    'pageMaxSize': '2000', # Get at max 2000 results for every term 
     'sortColumn': '',
     'sortDirection': ''
   },
@@ -76,10 +85,13 @@ reset_form_options = {
   'cookies': None
 }
 
-# Send the request to retrieve terms
+"""
+Retrieving available terms and their codes from the Banner
+"""
+# Send the request to retrieve terms:
 term_response = requests.get(**term_options)
 
-# Check if the request was successful
+# Check if the request was successful:
 if term_response.status_code == 200:
     upcoming_terms = term_response.json()
     # Setting cookies:
@@ -90,40 +102,52 @@ else:
     print('Failed to retrieve terms. Status code:', term_response.status_code)
 
 print(upcoming_terms)
-# all_course_details = []
 
-# for term in upcoming_terms:
-#     TERM_CODE = term['code']
-#     post_options['data']['term'] = TERM_CODE
-#     search_options['params']['txt_term'] = TERM_CODE
+"""
+Gathering courses and their descriptive features into a combined JSON file.
+"""
+all_course_details = []
 
-#     # POST to search for courses under the term
-#     post_response = requests.post(**post_options)
+# Looping through every term:
+for term in upcoming_terms:
+    TERM_CODE = term['code']
+    post_options['data']['term'] = TERM_CODE
+    search_options['params']['txt_term'] = TERM_CODE
 
-#     if post_response.status_code == 200:       
-#         print(post_response.json())
-#         search_response = requests.post(**search_options, allow_redirects=True)
-#         if search_response.status_code == 200:
-#             # Resolved
-#             all_course_details.append(search_response.json())
-#         else:
-#             print('Failed to search for courses. Status code:', search_response.status_code)
-#     else:
-#         print('Failed to POST to search for term. Status code:', post_response.status_code)
+    # POST to search for courses under the term:
+    post_response = requests.post(**post_options)
 
-# # Serializing json
-# json_object = json.dumps(all_course_details, indent=4)
+    # If successful:
+    if post_response.status_code == 200:       
+        print(post_response.json())
+        search_response = requests.post(**search_options, allow_redirects=True)
+        if search_response.status_code == 200:
+            # Append to the all_course_details list:
+            all_course_details.append(search_response.json())
+        else:
+            print('Failed to search for courses. Status code:', search_response.status_code)
+    else:
+        print('Failed to POST to search for term. Status code:', post_response.status_code)
+
+# Serializing json:
+json_object = json.dumps(all_course_details, indent=4)
  
-# # Writing to sample.json
-# with open("all_courses.json", "w") as outfile:
-#     outfile.write(json_object)
+# Writing to all_courses.json:
+with open("all_courses.json", "w") as outfile:
+    outfile.write(json_object)
 
+# Opening the json file:
 with open('all_courses.json') as f:
     data = json.load(f)
 
-# List to store normalized data
+"""
+Normalizing and transforming the nested JSON file to a Pandas dataframe. 
+Using only necessary columns. 
+"""
+# List to store normalized data:
 normalized_data = []
-# Extract relevant information from each JSON object
+
+# Extract relevant information from each JSON object:
 for obj in data:
     for course_obj in obj["data"]:
         course_info = {
@@ -145,7 +169,7 @@ for obj in data:
             "faculty_emailAddress": None
         }
         
-        # Check if the "faculty" list is not empty
+        # Check if the "faculty" list is not empty:
         if course_obj["faculty"]:
             course_info["faculty_banner_id"] = course_obj["faculty"][0]["bannerId"]
             course_info["faculty_displayName"] = course_obj["faculty"][0]["displayName"]
@@ -153,16 +177,32 @@ for obj in data:
         
         normalized_data.append(course_info)
 
+# Convert the list of dictionaries to a DataFrame:
 print(len(normalized_data))
-
-# Convert the list of dictionaries to a DataFrame
 df_normalized = pd.DataFrame(normalized_data)
 df_normalized['course_description'] = ''
-print(df_normalized.shape)
-# print(df_normalized.loc[0])
+print("Size of the dataframe:", df_normalized.shape)
 
+"""
+Getting course description using the getCourseDescription() POST request 
+for every CRN in the terms. 
+Adding the course description as a column. 
+"""
 # Function to perform the API request with retries and exponential backoff
 def perform_api_request_with_retry(api_request_func, **kwargs):
+    """
+        Parameters
+        ----------
+        api_request_func : method
+            The API request method (POST/GET)
+        options : **kwargs
+            The options (arguments) necessary for the request 
+        
+        Raises
+        ------
+        Exception
+            If response is not successful.
+    """
     max_retries = 3  # Maximum number of retries
     base_delay = 1   # Base delay in seconds
     max_delay = 32   # Maximum delay in seconds
@@ -184,19 +224,28 @@ def perform_api_request_with_retry(api_request_func, **kwargs):
     print("Exceeded maximum number of retries. Request failed.")
     return None
 
+# Looping through every course in the normalized dataframe:
 for index, course in df_normalized.iterrows():
+    # Updating term and CRN options for the course description retrieval:
     course_description_options['params']['term'] = course['term']
     course_description_options['params']['courseReferenceNumber']= course['courseReferenceNumber']
     description_response = perform_api_request_with_retry(requests.post, **course_description_options)
 
-    if description_response is not None and description_response.status_code == 200:
-        df_normalized.at[index, 'course_description'] = description_response.content
-        reset_response = perform_api_request_with_retry(requests.post, **reset_form_options)
-        if reset_response.status_code != 200:
-            print('Failed to reset search. Status code:', reset_response.status_code)
+    # If course description available after successful retries:
+    if description_response is not None:
+        if description_response.status_code == 200:
+            # Adding course description from the response content:
+            df_normalized.at[index, 'course_description'] = description_response.content
+            # Resetting the search session:
+            reset_response = perform_api_request_with_retry(requests.post, **reset_form_options)
+            if reset_response.status_code != 200:
+                print('Failed to reset search. Status code:', reset_response.status_code)
     else:
+        # Empty course description if unavailable:
         df_normalized.at[index, 'course_description'] = ''
-        print('Failed to get course description. Status code:', description_response.status_code)
+        print('Failed to get course description.', course['courseReferenceNumber'])
 
-# Writing to a csv file
+"""
+Writing to a csv file
+"""
 df_normalized.to_csv('banner_data.csv', index=True)
